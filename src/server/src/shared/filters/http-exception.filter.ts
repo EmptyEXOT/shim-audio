c64d87b2fc4f99773adbe610f7d5cc7c7a837b9a
EmptyEXOT <sadullaev.alim@mail.ru>
2024-11-20 21:37:50 +0300

    custom http exception filter for messages array response

---
+++ src/server/src/shared/filters/http-exception.filter.ts
@@ -0,0 +1,47 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    // Возможно понадобится при ведении журнала.
+    // const request = ctx.getRequest();
+
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let messages = ['Internal server error'];
+
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      const responseBody = exception.getResponse();
+
+      if (typeof responseBody === 'object' && 'message' in responseBody) {
+        messages = Array.isArray(responseBody.message)
+          ? responseBody.message
+          : [responseBody.message];
+      } else {
+        messages = [responseBody as string];
+      }
+    }
+
+    response.status(status).json({
+      statusCode: status,
+      error: HttpStatus[status],
+      messages: messages,
+      /* 
+        Возможно понадобится при ведении журнала. 
+        Пользователю эти данные лучше не отдавать
+      */
+      /*
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      */
+    });
+  }
+}
